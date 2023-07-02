f0f206848dd849be6e727897a0cde97ba7089c39
Akshay Chaudhary <124782758+AkshayChaudharyctrl@users.noreply.github.com>
2023-07-02 06:51:48 +0000

    first

--- src/components/App.js
+++ src/components/App.js
@@ -1,2 +1,96 @@
-<p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+import React, { useState, useEffect } from 'react';
 
+const reviewsData = [
+  {
+    id: 1,
+    image:
+      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-2_ipcjws.jpg',
+    name: 'maria ferguson',
+    title: 'office manager',
+    quote:
+      'Fingerstache umami squid, kinfolk subway tile selvage tumblr man braid viral kombucha gentrify fanny pack raclette pok pok mustache.',
+  },
+  {
+    id: 2,
+    image:
+      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg',
+    name: 'john doe',
+    title: 'regular guy',
+    quote:
+      'Gastropub sustainable tousled prism occupy. Viral XOXO roof party brunch actually, chambray listicle microdosing put a bird on it paleo subway tile squid umami.',
+  },
+  {
+    id: 3,
+    image:
+      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959121/person-1_aufeoq.jpg',
+    name: 'peter smith',
+    title: 'product designer',
+    quote:
+      'Drinking vinegar polaroid street art echo park, actually semiotics next level butcher master cleanse hammock flexitarian ethical paleo.',
+  },
+  {
+    id: 4,
+    image:
+      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg',
+    name: 'susan andersen',
+    title: 'the boss',
+    quote:
+      'Marfa af yr 3 wolf moon kogi, readymade distillery asymmetrical seitan kale chips fingerstache cloud bread mustache twee messenger bag. ',
+  },
+];
+
+const App = () => {
+  const [index, setIndex] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setIndex((prevIndex) => (prevIndex + 1) % reviewsData.length);
+    }, 3000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
+  const handleNext = () => {
+    setIndex((prevIndex) => (prevIndex + 1) % reviewsData.length);
+  };
+
+  const handlePrev = () => {
+    setIndex((prevIndex) =>
+      prevIndex === 0 ? reviewsData.length - 1 : prevIndex - 1
+    );
+  };
+
+  return (
+    <div>
+      <h1 id="review-heading">Reviews</h1>
+      <div id="review-container">
+        <div className="slider">
+          {reviewsData.map((review, idx) => (
+            <div
+              key={review.id}
+              className={idx === index ? 'slide active' : 'slide'}
+            >
+              <img
+                id={`person-${idx}-image`}
+                src={review.image}
+                alt={review.name}
+              />
+              <h2 id={`person-${idx}-name`}>{review.name}</h2>
+              <p>{review.title}</p>
+              <p>{review.quote}</p>
+            </div>
+          ))}
+        </div>
+        <button className="prev" onClick={handlePrev}>
+          Prev
+        </button>
+        <button className="next" onClick={handleNext}>
+          Next
+        </button>
+      </div>
+      </div>
+)}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
+import { render } from "enzyme";
 
+ReactDOM.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
 
 
